7e7387b441bfd67877c58520a7f0dace16d33e69
Henry Patterson <henrycpatterson92@gmail.com>
2021-07-14 12:36:12 -0400

    cycle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import {Route} from "react-router-dom"
 import './App.css';
+import { Route } from "react-router-dom"
+import Home from "./components/Home"
+import Navbar from './components/Navbar';
+import Maintenance from './components/Maintenance';
+import Safety from './components/Safety';
+import NewTips from './components/NewTips';
 
 function App() {
   return (
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function Home() {
+
+}
